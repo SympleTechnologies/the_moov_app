caeceb8b868a7a69e6cbb3cdf91d732cb21e4013
Ofor Chinedu <oforchinedukelechi@gmail.com>
2018-06-10 17:43:22 +0100

    finish chore

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>MOOV</Text>
+        <Text>MOOV </Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
